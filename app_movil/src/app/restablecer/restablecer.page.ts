185114055a467e6ed3f164ef5000ab2d6a701fcd
diegoxvega <127541258+diegoxvega@users.noreply.github.com>
2024-09-21 17:39:52 -0300

    V0.1 de app

---
+++ app_movil/src/app/restablecer/restablecer.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restablecer',
+  templateUrl: './restablecer.page.html',
+  styleUrls: ['./restablecer.page.scss'],
+})
+export class RestablecerPage implements OnInit {
+
+username: string = 'guest'
+
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
